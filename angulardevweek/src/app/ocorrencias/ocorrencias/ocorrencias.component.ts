5f99d80d9eda5e84a966272f087e23e5db4b2054
Jose Milton Junior <jmjuniormonteiro@gmail.com>
2022-03-11 11:42:33 -0300

    Criando o model e service de Ocorrência

--- angulardevweek/src/app/ocorrencias/ocorrencias/ocorrencias.component.ts
+++ angulardevweek/src/app/ocorrencias/ocorrencias/ocorrencias.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Ocorrencia } from '../model/ocorrencia';
 import { Regiao } from '../model/regiao';
+import { OcorrenciaService } from '../service/ocorrencia.service';
 import { RegiaoService } from '../service/regiao.service';
 
 
@@ -8,14 +10,19 @@ import { RegiaoService } from '../service/regiao.service';
   templateUrl: './ocorrencias.component.html',
   styleUrls: ['./ocorrencias.component.css']
 })
-export class OcorrenciasComponent implements OnInit {
+export class OcorrenciasComponent implements OnInit{
 
+  ocorrencia_exame: Ocorrencia[] = [];
   regioes: Regiao[] = [];
 
-  constructor(private regiaoService:RegiaoService) { }
+  constructor(
+    private ocorrenciaService: OcorrenciaService,
+    private regiaoService: RegiaoService
+    ) { }
 
   ngOnInit(): void {
     this.regioes = this.regiaoService.listRegioes();
+    this.ocorrencia_exame = this.ocorrenciaService.listOcorrencias();
   }
 
 }
